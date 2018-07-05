9ff4cd7084863c40d684514bc112a6ac9d8b6df4
Liam Neville <lineville@vassar.edu>
2018-07-05 12:41:50 -0400

    basic command line version done

--- game/index.js
+++ game/index.js
@@ -1,6 +1,22 @@
-import {Map} from 'immutable'
+import { Map } from 'immutable'
 
-export default function reducer(state, action) {
-  // TODO
-  return state
+const MOVE = 'move'
+
+const switchTurn = turn => {
+  return turn === 'X' ? 'O' : 'X'
+}
+export function move(turn, position) {
+  return { type: MOVE, turn, position }
+}
+
+export default function reducer(state = { turn: 'X', board: Map() }, action) {
+  switch (action.type) {
+    case MOVE:
+      return {
+        turn: switchTurn(state.turn),
+        board: state.board.setIn(action.position, action.turn),
+      }
+    default:
+      return state
+  }
 }
